9802f1ef596c8ea15b031195392c5d79f2e23846
NurUcar <nur0ucar@gmail.com>
2020-06-03 21:08:59 +0300

    Updated Categories with Django MPTT Library

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
+
 from product.models import Category, Product, Images
 
 
@@ -27,6 +29,38 @@ class ImagesAdmin(admin.ModelAdmin):
     readonly_fields = ('image_tag',)
 
 
-admin.site.register(Category, CategoryAdmin)
+class CategoryAdmin2(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    list_display = ('tree_actions', 'indented_title',
+                    'related_products_count', 'related_products_cumulative_count')
+    list_display_links = ('indented_title',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = Category.objects.add_related_count(
+            qs,
+            Product,
+            'category',
+            'products_cumulative_count',
+            cumulative=True)
+        qs = Category.objects.add_related_count(qs,
+                                                Product,
+                                                'category',
+                                                'products_count',
+                                                cumulative=False)
+        return qs
+
+    def related_products_count(self, instance):
+        return instance.products_count
+
+    related_products_count.short_description = 'Related products (for this specific category)'
+
+    def related_products_cumulative_count(self, instance):
+        return instance.products_cumulative_count
+
+    related_products_cumulative_count.short_description = 'Related product (in tree)'
+
+
+admin.site.register(Category, CategoryAdmin2)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Images, ImagesAdmin)
