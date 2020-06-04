195c1fed0104f755e5bf917144e6236f09edb11c
NurUcar <nur0ucar@gmail.com>
2020-06-04 21:44:36 +0300

    Product page added

--- home/views.py
+++ home/views.py
@@ -14,27 +14,32 @@ def index(request):
     context = {'setting': setting,
                'category': category,
                'page': 'home',
-               'sliderdata': sliderdata}
+               'sliderdata': sliderdata, }
     return render(request, 'index.html', context)
 
 
 def aboutus(request):
     setting = Setting.objects.get(pk=1)
+    category = Category.objects.all()
     context = {'setting': setting,
-               'page': 'aboutus'}
+               'page': 'aboutus',
+               'category': category, }
     return render(request, 'aboutus.html', context)
 
 
 def references(request):
     setting = Setting.objects.get(pk=1)
+    category = Category.objects.all()
     context = {'setting': setting,
-               'page': 'references'}
+               'page': 'references',
+               'category': category, }
     return render(request, 'references.html', context)
 
 
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
+
         if form.is_valid():
             data = ContactFormMessage()
             data.name = form.cleaned_data['name']
@@ -47,6 +52,24 @@ def contact(request):
             return HttpResponseRedirect('/contact')
 
     setting = Setting.objects.get(pk=1)
+    category = Category.objects.all()
     form = ContactForm()
-    context = {'setting': setting, 'form': form}
+    context = {'setting': setting,
+               'form': form,
+               'category': category,}
     return render(request, 'contact.html', context)
+
+
+def category_products(request, id, slug):
+    category = Category.objects.all()
+    categorydata = Category.objects.get(pk=id)
+    productCount = Product.objects.filter(category_id=id).count()
+    products = Product.objects.filter(category_id=id)
+    setting = Setting.objects.get(pk=1)
+    context = {'products': products,
+               'category': category,
+               'categorydata': categorydata,
+               'productCount': productCount,
+               'setting': setting,
+               }
+    return render(request, 'product.html', context)
