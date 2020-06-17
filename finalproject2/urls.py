9dcb93be81b5fe4ab85a9fe9c0757c93b4beac3a
NurUcar <nur0ucar@gmail.com>
2020-06-17 17:25:38 +0300

    Shopcart Product Add and delete

--- finalproject2/urls.py
+++ finalproject2/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from home import views
+from order import views as orderviews
 
 urlpatterns = [
 
@@ -25,6 +26,7 @@ urlpatterns = [
     path('home/', include('home.urls')),
     path('product/', include('product.urls')),
     path('user/', include('user.urls')),
+    path('order/', include('order.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
 
@@ -39,6 +41,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout_view'),
     path('login/', views.login_view, name='login_view'),
     path('signup/', views.signup_view, name='signup_view'),
+    path('shopcart/', orderviews.shopcart, name='shopcart'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ order/views.py
@@ -0,0 +1,80 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+# Create your views here.
+from home.models import Setting
+from order.models import ShopCartForm, ShopCart
+from product.models import Category
+
+
+def index(request):
+    return HttpResponse("Order App")
+
+
+@login_required(login_url='login')
+def addtocart(request, id):
+    url = request.META.get('HTTP_REFERER')
+    current_user = request.user
+    checkproduct = ShopCart.objects.filter(product_id=id)
+    if checkproduct:
+        control = 1
+    else:
+        control = 0
+    if request.method == 'POST':
+        form = ShopCartForm(request.POST)
+        if form.is_valid():
+            if control == 1:
+                data = ShopCart.objects.get(product_id=id)
+                data.quantity += form.cleaned_data['quantity']
+                data.save()
+            else:
+                data = ShopCart()
+                data.user_id = current_user.id
+                data.product_id = id
+                data.quantity = form.cleaned_data['quantity']
+                data.save()
+            messages.success(request, "Ürün başarı ile sepete eklenmiştir. Teşekkür Ederiz.")
+            return HttpResponseRedirect(url)
+
+    else:
+        if control == 1:
+            data = ShopCart.objects.get(product_id=id)
+            data.quantity += 1
+            data.save()
+        else:
+            data = ShopCart()
+            data.user_id = current_user.id
+            data.product_id = id
+            data.quantity = 1
+            data.save()
+        messages.success(request, "Ürün başarı ile sepete eklenmiştir. Teşekkür Ederiz.")
+        return HttpResponseRedirect(url)
+    messages.warning(request, "Ürün sepete eklenemedi!. Lütfen kontrol ediniz..")
+    return HttpResponseRedirect(url)
+
+
+@login_required(login_url='/login')
+def shopcart(request):
+    category = Category.objects.all()
+    setting = Setting.objects.get(pk=1)
+
+    current_user = request.user
+    shop_cart = ShopCart.objects.filter(user_id=current_user.id)
+    total = 0
+    for rs in shop_cart:
+        total += rs.product.price * rs.quantity
+    context = {'shop_cart': shop_cart,
+               'category': category,
+               'total': total,
+               'setting': setting,
+               }
+    return render(request, 'Shopcart_products.html', context)
+
+
+@login_required(login_url='/login')
+def deletefromcart(request, id):
+    ShopCart.objects.filter(id=id).delete()
+    messages.success(request, "Ürün başarılı bir şekilde sepetten kaldırılmıştır.")
+    return HttpResponseRedirect("/shopcart")
